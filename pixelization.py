4d006f3e726927061d6abe56699bf224c1f9a35d
Larina Chen <larinachen2024@u.northwestern.edu>
2023-10-31 16:51:28 -0500

    add gaussian noise

--- pixelization.py
+++ pixelization.py
@@ -3,26 +3,28 @@ import math
 import numpy as np
 import json
 
-# get license plate bounding box:
-with open('data/bounding_box.json', 'r') as bounding_box_data_file:
-    bounding_box_data = json.load(bounding_box_data_file)
+def main():
+    # get license plate bounding box:
+    with open('data/bounding_box.json', 'r') as bounding_box_data_file:
+        bounding_box_data = json.load(bounding_box_data_file)
 
-# noise up each image 
-for carID, bounding_box, in bounding_box_data.items():
+    # noise up each image 
+    for carID, bounding_box, in bounding_box_data.items():
 
-    # file name to open and file name to write out
-    file_name = f'data/images/{carID}.png'
-    gray_file_name = f'gray_{carID}.png'
+        # file name to open and file name to write out
+        file_name = f'data/images/{carID}.png'
+        gray_file_name = f'gray_{carID}.png'
 
-    # open image
-    image = Image.open(file_name)
+        # open image
+        image = Image.open(file_name)
 
-    # convert image to grayscale
-    gray_image = ImageOps.grayscale(image)
+        # convert image to grayscale
+        gray_image = ImageOps.grayscale(image)
 
-    # save grayscale image for reference - not used
-    # gray_image.save(gray_file_name)
+        pixelate_one_image(carID, gray_image, bounding_box, "gaussian")
 
+
+def pixelate_one_image(carID, gray_image, bounding_box, mode):
     # get bounding box dimensions
     xmin, xmax, ymin, ymax = bounding_box["xmin"], bounding_box["xmax"], bounding_box["ymin"], bounding_box["ymax"]
     license_plate_width = xmax - xmin
@@ -30,10 +32,9 @@ for carID, bounding_box, in bounding_box_data.items():
     
     # compute grid dimensions
     num_cells = 16
+    epsilon = 0.5
     cell_width = math.ceil(license_plate_width/num_cells)
     cell_height = math.ceil(license_plate_height/num_cells)
-
-    cells = [[0 for r in range(1,num_cells)] for c in range(1,num_cells)]
 
     # Calculate the noised up average color value for each cell
     # Loop through all cells
@@ -55,9 +56,14 @@ for carID, bounding_box, in bounding_box_data.items():
             num_pixels = min(cell_width, xmax - xmin) * min(cell_height, ymax - ymin)
             cell_average = cell_sum / num_pixels
 
-            # Add noise
-            noise = np.random.laplace(0, 255 / (num_cells * num_cells))
-            cell_average += noise
+            if mode == "laplace":
+                # Add laplace noise
+                lp_noise = np.random.laplace(0, 255 / (num_cells * num_cells * epsilon))
+                cell_average += lp_noise
+            if mode == "gaussian":
+                gaus_noise = np.random.normal(0, 255 / (num_cells * num_cells))
+                cell_average += gaus_noise
+
 
             # Ensure the cell value is within [0, 255]
             cell_average = max(0, min(255, cell_average))
@@ -72,5 +78,12 @@ for carID, bounding_box, in bounding_box_data.items():
                     if pixel_x < xmax and pixel_y < ymax:
                         gray_image.putpixel((pixel_x, pixel_y), round(cell_average))
 
-    # Save pixelated image
-    gray_image.save(f"output/noise_{carID}.png")
+    # Save pixelated laplace image
+    if mode == "laplace":
+        gray_image.save(f"output/laplace_noise_{carID}.png")
+    # Save pixelated gaussian image
+    if mode == "gaussian":
+        gray_image.save(f"output/gaussian_noise_{carID}.png")
+
+
+main()
